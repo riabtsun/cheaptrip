26f52b5f2ac1fb18611faf1f69b7409716f9b17f
dryabtsun <riabtsun@i.ua>
2023-05-30 15:35:20 +0300

    added new text

--- src/App.js
+++ src/App.js
@@ -6,17 +6,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>
+          Hello world
+        </h2>
+        <p style={{padding: '0 40px'}}>Hello! My name is Dmytro I got acquainted with the possibility of internship offered by your company.
+          I have commercial experience of efficiently coding websites using modern HTML, CSS and JavaScript, jQuery and also delivered on Wordpress and Opencart. I am also actively studying React, which I have not yet used in commercial projects. In addition to my knowledge base, I actively seek out new technologies and stay on top of industry trends and advancements, quickly learning the material I need. I really want to develop professionally and increase my knowledge level.
+
+          Thank you for giving me your time.</p>
       </header>
     </div>
   );
